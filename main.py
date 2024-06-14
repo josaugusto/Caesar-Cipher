1be234e079cf7c6d5350391edf974a477ccf59ad
José Augusto <79456742+josaugusto@users.noreply.github.com>
2024-06-14 08:44:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
-    
+
+
 def novo_alfabeto(chave, deslocamento):
     if deslocamento == 'D':
         chave = -chave
     return alfabeto[chave:] + alfabeto[:chave]
 
-def criptografar(mensagem, chave, deslocamento):
-    alfabeto_deslocado = novo_alfabeto(chave, deslocamento)
-    mensagem_criptografada = ""
-
+def aux_cripto_descripto(mensagem, alfabeto, alfabeto_deslocado):
     for j in range(0, len(mensagem)):
         for i in range(0, 26):
             if mensagem[j].upper() == alfabeto[i]:
                 if mensagem[j].isupper():
-                    mensagem_criptografada+=alfabeto_deslocado[i]
+                    mensagem_processada+=alfabeto_deslocado[i]
                 else:
-                    mensagem_criptografada+=alfabeto_deslocado[i].lower()
-        if mensagem[j] == ' ' or mensagem[j] == '\n': mensagem_criptografada+=mensagem[j]
-    return mensagem_criptografada
+                    mensagem_processada+=alfabeto_deslocado[i].lower()
+        if mensagem[j] == ' ' or mensagem[j] == '\n': mensagem_processada+=mensagem[j]
+    return mensagem_processada
+
+def criptografar(mensagem, chave, deslocamento):
+    alfabeto_deslocado = novo_alfabeto(chave, deslocamento)
+
+    return aux_cripto_descripto(mensagem, alfabeto, alfabeto_deslocado);
 
 def descriptografar(mensagem_cifrada, chave, deslocamento):
     alfabeto_deslocado = novo_alfabeto(chave, deslocamento)
-    mensagem_descriptografada = ""
     
-    for j in range(0, len(mensagem_cifrada)):
-        for i in range(0, 26):
-            if mensagem_cifrada[j].upper() == alfabeto_deslocado[i]:
-                if mensagem_cifrada[j].isupper():
-                    mensagem_descriptografada+=alfabeto[i]
-                else:
-                    mensagem_descriptografada+=alfabeto[i].lower()
-        if mensagem_cifrada[j] == ' ' or mensagem_cifrada[j] == '\n': mensagem_descriptografada+=mensagem_cifrada[j]
-    return mensagem_descriptografada
+    return aux_cripto_descripto(mensagem_cifrada, alfabeto_deslocado, alfabeto)
 
 def criptoanalise(mensagem_cifrada):
     for k in range(0, 26):
